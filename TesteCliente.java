bb0ceb25858c8355fe513934c092c0529a22c855
KingDavid14k <110508939+KingDavid14k@users.noreply.github.com>
2022-09-05 20:50:50 -0300

    Update TesteCliente.java
    criando classe cliente

--- TesteCliente.java
+++ TesteCliente.java
@@ -3,5 +3,22 @@ public static void main(String [] args){
 
 System.out.println("Processo o cliente...")
 }
+  class cliente{
+    String nome;
+    String cpf;
 
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+    public String getNome(){
+      return this.nome;
+      
+    }
+      
+public void setCpf(){
+  this.cpf = cpf;
+  }
+    public String getCpf(){
+      return this.cpf;
+    }
 }
